192c4d836471c5d8ee5da6a1b8c70cdd83fa7e56
yimei-wu <wuyimei60@gmail.com>
2024-04-11 16:07:03 +0200

    ad link and id

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-console.log("JAVASCRIPT");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        console.log(this.getAttribute('href'));
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
